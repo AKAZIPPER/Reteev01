5c70f41cd453a317d8bf7a2ef3a4be89296012d9
Donghyeon Kim <dhkd4613@naver.com>
2023-05-12 17:20:39 +0900

    게시판 페이지네이션 처리, 검색기능 추가

--- src/main/java/com/project/reteev/board/controller/BoardController.java
+++ src/main/java/com/project/reteev/board/controller/BoardController.java
@@ -1,19 +1,18 @@
 package com.project.reteev.board.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.project.reteev.board.dto.BoardDto;
 import com.project.reteev.board.entity.BoardEntity;
 import com.project.reteev.board.repository.BoardRepository;
 
@@ -25,9 +24,17 @@ public class BoardController {
 	private BoardRepository boardRepository;
 	
 	@GetMapping("/list")
-	public String list(@RequestParam(required = false) String message, Model model) {
-		List<BoardEntity> board = boardRepository.findAll();
-		
+	public String list(@RequestParam(required = false) String message,
+						Model model, 
+						@PageableDefault(size = 4) Pageable  pageable,
+						@RequestParam(required = false, defaultValue = "") String searchText) 
+	{
+//		Page<BoardEntity> board =  boardRepository.findAll(pageable);
+		Page<BoardEntity> board =  boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+		int startPage = Math.max(1, board.getPageable().getPageNumber() - 14); 
+		int endPage = Math.min(board.getTotalPages(), board.getPageable().getPageNumber() + 14);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("message", message);
 		model.addAttribute("lists", board);
 		
--- src/main/java/com/project/reteev/board/repository/BoardRepository.java
+++ src/main/java/com/project/reteev/board/repository/BoardRepository.java
@@ -1,11 +1,11 @@
 package com.project.reteev.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.project.reteev.board.entity.BoardEntity;
 
-
-@EnableJpaRepositories
 public interface BoardRepository extends JpaRepository<BoardEntity, Long>{
-		
+	
+	Page<BoardEntity> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
